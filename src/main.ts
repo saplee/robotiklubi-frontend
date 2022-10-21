7c1d0b4a87fcf5005426e5c182e2992ff7b4272f
Rainer Viirlaid <raviir@ttu.ee>
2022-10-21 17:31:17 +0300

    Implemented markdown rendering system and created a simple testing page.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-
+// import {marked} from 'marked';
+// const markedMixin = {
+//     methods: {
+//         md: function (input: string): string {
+//             return marked(input);
+//         },
+//     },
+// };
 const app = createApp(App)
 
 app.use(router)
+
+// app.mixin(markedMixin)
 
 app.mount('#app')
---
+++ src/views/MarkView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <textarea v-model="markdown"></textarea>
+    <div v-html="markdownToHtml"></div>
+  </div>
+</template>
+<script>
+import {marked} from 'marked';
+export default {
+  name: "Markdown",
+  data() {
+    return {
+      markdown: " # hello world ",
+    };
+  },
+  computed: {
+    markdownToHtml() {
+      return marked.parse(this.markdown);
+    },
+  }
+};
+</script>
+<style>
+</style>
