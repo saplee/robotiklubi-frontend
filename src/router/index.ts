e2fb3e6337bb1d966e3a1262bfb09ff7cbc80622
saplee <saplee@ttu.ee>
2022-10-30 13:25:20 +0200

    About Page fix 3

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../components/SocialMedia.vue')
+      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/markdown',
