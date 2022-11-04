95a3817d5364a4218a26740419b7999d471d2d8f
Rainer Viirlaid <raviir@ttu.ee>
2022-11-04 13:45:49 +0200

    Implemented initial navbar.

--- src/App.vue
+++ src/App.vue
@@ -4,84 +4,222 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <nav>
+    <div id="nav-bar">
+      <div class="nav-logo-container">
+        <a href='#'>
+          <img src="@/assets/rk_logo.png" alt="club logo">
+        </a>
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="nav-item">
+        <p class="nav-item-title" >Teenused</p>
+      </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/markdown">Markdown</RouterLink>
-        <RouterLink to="/wiki">Wiki</RouterLink>
-      </nav>
+      <div class="nav-item">
+        <p class="nav-item-title" >Listie</p>
+        <ul class="dropdown">
+          <li><a href="">Stuff</a></li>
+          <li><a href="">More Stuff</a></li>
+          <li><hr></li>
+          <li><a href="">Separate Stuff</a></li>
+        </ul>
+      </div>
+
+      <div class="nav-item">
+        <p class="nav-item-title" >Listie_2</p>
+        <ul class="dropdown">
+          <li><a href="">2019</a></li>
+          <li><hr></li>
+          <li><a href="">2018</a></li>
+          <li><a href="">2017</a></li>
+        </ul>
+      </div>
+
+      <div class="nav-item">
+        <p class="nav-item-title" >Ready Views</p>
+        <ul class="dropdown">
+          <li><RouterLink class="nav-link" to="/">Home</RouterLink></li>
+          <li><RouterLink class="nav-link" to="/about">About</RouterLink></li>
+          <li><RouterLink class="nav-link" to="/markdown">Markdown</RouterLink></li>
+          <li><RouterLink class="nav-link" to="/wiki">Wiki</RouterLink></li>
+        </ul>
+      </div>
+
+      <div class="nav-item">
+        <p class="nav-item-title" >Huvilisele</p>
+      </div>
+
+      <div class="nav-item">
+        <p class="nav-item-title" >Wiki</p>
+      </div>
+
+      <div class="nav-item">
+        <p class="nav-item-title" >Klubist</p>
+      </div>
+
     </div>
-  </header>
+  </nav>
+  <!--  <header>-->
+  <!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+
+  <!--    <div class="wrapper">-->
+  <!--      <HelloWorld msg="You did it!" />-->
+
+  <!--      <nav>-->
+  <!--        <RouterLink to="/">Home</RouterLink>-->
+  <!--        <RouterLink to="/about">About</RouterLink>-->
+  <!--        <RouterLink to="/markdown">Markdown</RouterLink>-->
+  <!--        <RouterLink to="/wiki">Wiki</RouterLink>-->
+  <!--      </nav>-->
+  <!--    </div>-->
+  <!--  </header>-->
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+/*header {*/
+/*  line-height: 1.5;*/
+/*  max-height: 100vh;*/
+/*}*/
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+/*.logo {*/
+/*  display: block;*/
+/*  margin: 0 auto 2rem;*/
+/*}*/
 
-nav {
+/*nav {*/
+/*  width: 100%;*/
+/*  font-size: 12px;*/
+/*  text-align: center;*/
+/*  margin-top: 2rem;*/
+/*}*/
+
+/*nav a.router-link-exact-active {*/
+/*  color: var(--color-text);*/
+/*}*/
+
+/*nav a.router-link-exact-active:hover {*/
+/*  background-color: transparent;*/
+/*}*/
+
+/*nav a {*/
+/*  display: inline-block;*/
+/*  padding: 0 1rem;*/
+/*  border-left: 1px solid rgba(60, 60, 60, 0.29);*/
+/*}*/
+
+/*nav a:first-of-type {*/
+/*  border: 0;*/
+/*}*/
+
+/*@media (min-width: 1024px) {*/
+/*  header {*/
+/*    display: flex;*/
+/*    place-items: center;*/
+/*    padding-right: calc(160px / 2);*/
+/*  }*/
+
+/*  .logo {*/
+/*    margin: 0 2rem 0 0;*/
+/*  }*/
+
+/*  header .wrapper {*/
+/*    display: flex;*/
+/*    place-items: flex-start;*/
+/*    flex-wrap: wrap;*/
+/*  }*/
+
+/*  nav {*/
+/*    text-align: left;*/
+/*    margin-left: -1rem;*/
+/*    font-size: 1rem;*/
+
+/*    padding: 1rem 0;*/
+/*    margin-top: 1rem;*/
+/*  }*/
+/*}*/
+
+
+#nav-bar {
+  display: flex;
+  height: 4rem;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  background: var(--color-navbar);
 }
 
-nav a.router-link-exact-active {
+.nav-logo-container {
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.nav-logo-container img {
+  height: 100%;
+}
+
+#nav-bar p {
+  /*float: left;*/
+  padding: 1rem 2rem;
+  height: 100%;
+  font-weight: 600;
   color: var(--color-text);
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+#nav-bar .nav-item:hover {
+  background: var(--color-accent);
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.nav-item {
+  position: relative;
+  cursor: pointer;
+  text-decoration: none;
 }
 
-nav a:first-of-type {
-  border: 0;
+.dropdown {
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility var(--transition-duration), opacity var(--transition-duration);
+  background: var(--color-navbar);
+  max-width: 100%;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  cursor: auto;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.nav-item:hover .dropdown {
+  visibility: visible;
+  opacity: 1;
+  /*padding-bottom: 1rem;*/
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.nav-item li {
+  padding: 0.5rem 1rem;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.nav-link {
+  display: block;
+  text-decoration: none;
+  /*padding: 1rem 2rem;*/
+  height: 100%;
+  color: var(--color-accent);
+  /*outline: 2px solid red;*/
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+.dropdown a {
+  text-decoration: none;
+}
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.dropdown li:hover {
+  background: var(--color-navbar-dropdown-highlight);
+}
+
+.dropdown hr {
+  border: 1px solid var(--color-accent);
+  width: 100%;
+  margin: 0 auto;
 }
 </style>
--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -43,14 +43,14 @@ h3 {
 @media (min-width: 1024px) {
   .item {
     margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    padding: 0.4rem 0 1rem calc(160px / 2);
   }
 
   i {
     top: calc(50% - 25px);
     left: -26px;
     position: absolute;
-    border: 1px solid var(--color-border);
+    border: 1px solid rgba(60, 60, 60, 0.29);
     background: var(--color-background);
     border-radius: 8px;
     width: 50px;
@@ -59,7 +59,7 @@ h3 {
 
   .item:before {
     content: ' ';
-    border-left: 1px solid var(--color-border);
+    border-left: 1px solid rgba(60, 60, 60, 0.29);
     position: absolute;
     left: 0;
     bottom: calc(50% + 25px);
@@ -68,7 +68,7 @@ h3 {
 
   .item:after {
     content: ' ';
-    border-left: 1px solid var(--color-border);
+    border-left: 1px solid rgba(60, 60, 60, 0.29);
     position: absolute;
     left: 0;
     top: calc(50% + 25px);
