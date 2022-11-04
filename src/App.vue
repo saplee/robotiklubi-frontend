d54503ac3aea6d6d2b2b8a1435321c7b83909bdb
Rainer Viirlaid <raviir@ttu.ee>
2022-11-04 15:05:15 +0200

    Changed favicon and started work on footer.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <div id="nav-bar">
       <div class="nav-logo-container">
         <a href='#'>
-          <img src="@/assets/rk_logo.png" alt="club logo">
+          <img src="@/assets/rk_logo.svg" alt="club logo">
         </a>
       </div>
 
@@ -60,86 +59,18 @@ import HelloWorld from './components/HelloWorld.vue'
 
     </div>
   </nav>
-  <!--  <header>-->
-  <!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
-
-  <!--    <div class="wrapper">-->
-  <!--      <HelloWorld msg="You did it!" />-->
-
-  <!--      <nav>-->
-  <!--        <RouterLink to="/">Home</RouterLink>-->
-  <!--        <RouterLink to="/about">About</RouterLink>-->
-  <!--        <RouterLink to="/markdown">Markdown</RouterLink>-->
-  <!--        <RouterLink to="/wiki">Wiki</RouterLink>-->
-  <!--      </nav>-->
-  <!--    </div>-->
-  <!--  </header>-->
 
   <RouterView />
+
+  <footer>
+    <h1>Footer</h1>
+  </footer>
 </template>
 
 <style scoped>
-/*header {*/
-/*  line-height: 1.5;*/
-/*  max-height: 100vh;*/
-/*}*/
 
-/*.logo {*/
-/*  display: block;*/
-/*  margin: 0 auto 2rem;*/
-/*}*/
 
-/*nav {*/
-/*  width: 100%;*/
-/*  font-size: 12px;*/
-/*  text-align: center;*/
-/*  margin-top: 2rem;*/
-/*}*/
-
-/*nav a.router-link-exact-active {*/
-/*  color: var(--color-text);*/
-/*}*/
-
-/*nav a.router-link-exact-active:hover {*/
-/*  background-color: transparent;*/
-/*}*/
-
-/*nav a {*/
-/*  display: inline-block;*/
-/*  padding: 0 1rem;*/
-/*  border-left: 1px solid rgba(60, 60, 60, 0.29);*/
-/*}*/
-
-/*nav a:first-of-type {*/
-/*  border: 0;*/
-/*}*/
-
-/*@media (min-width: 1024px) {*/
-/*  header {*/
-/*    display: flex;*/
-/*    place-items: center;*/
-/*    padding-right: calc(160px / 2);*/
-/*  }*/
-
-/*  .logo {*/
-/*    margin: 0 2rem 0 0;*/
-/*  }*/
-
-/*  header .wrapper {*/
-/*    display: flex;*/
-/*    place-items: flex-start;*/
-/*    flex-wrap: wrap;*/
-/*  }*/
-
-/*  nav {*/
-/*    text-align: left;*/
-/*    margin-left: -1rem;*/
-/*    font-size: 1rem;*/
-
-/*    padding: 1rem 0;*/
-/*    margin-top: 1rem;*/
-/*  }*/
-/*}*/
+/*NAVBAR*/
 
 
 #nav-bar {
@@ -156,11 +87,11 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 
 .nav-logo-container img {
+  padding: 7px;
   height: 100%;
 }
 
 #nav-bar p {
-  /*float: left;*/
   padding: 1rem 2rem;
   height: 100%;
   font-weight: 600;
@@ -193,7 +124,6 @@ import HelloWorld from './components/HelloWorld.vue'
 .nav-item:hover .dropdown {
   visibility: visible;
   opacity: 1;
-  /*padding-bottom: 1rem;*/
 }
 
 .nav-item li {
@@ -203,10 +133,8 @@ import HelloWorld from './components/HelloWorld.vue'
 .nav-link {
   display: block;
   text-decoration: none;
-  /*padding: 1rem 2rem;*/
   height: 100%;
   color: var(--color-accent);
-  /*outline: 2px solid red;*/
 }
 
 .dropdown a {
@@ -221,5 +149,25 @@ import HelloWorld from './components/HelloWorld.vue'
   border: 1px solid var(--color-accent);
   width: 100%;
   margin: 0 auto;
+}
+
+
+/*FOOTER*/
+
+
+main {
+  padding-bottom: 4rem;
+}
+
+footer {
+  background-color: var(--color-navbar);
+  position: absolute;
+  /*flex-shrink: 0;*/
+  /*height: 4rem;*/
+  /*padding-top: 4rem;*/
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  align-items: center;
 }
 </style>
