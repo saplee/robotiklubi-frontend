b3420d4287be1f3afc22b276fefc02656ec18ff7
Rainer Viirlaid <raviir@ttu.ee>
2022-11-30 00:01:36 +0200

    Tags can now be added to a new page.

--- src/views/WikiCreateView.vue
+++ src/views/WikiCreateView.vue
@@ -3,25 +3,49 @@
     <div id="wiki-create-container">
       <MarkdownEditorComponent></MarkdownEditorComponent>
       <div class="secondary-container shadowed">
-        <TagSelectorComponent></TagSelectorComponent>
-        <button>Create</button>
+        <TagSelectorComponent ref="tagSelector" :page-id="-1"></TagSelectorComponent>
+        <button v-on:click="createPage">Create</button>
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, onMounted, ref} from "vue";
 import MarkdownEditorComponent from "@/components/wiki/MarkdownEditorComponent.vue";
 import TagSelectorComponent from "@/components/wiki/TagSelectorComponent.vue";
 
 export default defineComponent({
   name: "WikiCreateView",
   components: {MarkdownEditorComponent, TagSelectorComponent},
+  setup() {
+    const tagSelector = ref()
+    return {
+      tagSelector,
+    };
+  },
   data() {
     return {
       title: "",
       content: ""
+    }
+  },
+  methods: {
+    createPage: function () {
+      console.log()
+      console.log("TO ADD")
+      const tagsToAdd = this.tagSelector.getTagsToAdd()
+      for (let i = 0; i < tagsToAdd.length; i++) {
+        console.log(tagsToAdd[i].tag)
+      }
+      console.log()
+      console.log("TO REMOVE")
+      const tagsToRemove = this.tagSelector.getTagsToRemove()
+      for (let i = 0; i < tagsToRemove.length; i++) {
+        console.log(tagsToRemove[i].tag)
+      }
+      console.log(JSON.parse(JSON.stringify(this.tagSelector.getTagsToAdd())))
+      console.log(this.tagSelector.getTagsToAdd())
     }
   }
 })
@@ -40,7 +64,7 @@ export default defineComponent({
 .secondary-container {
   margin: 0;
   height: fit-content;
-  width: fit-content;
+  width: 15rem;
 }
 
 button {
