3e8b016692b2499be4c9a5f1f9c97af19cb4f624
alvaht <alvaht@ttu.ee>
2022-12-02 11:56:00 +0200

    Revert google maps api changes due to cost and use embedded google maps as an iframe

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -17,6 +17,9 @@
 
     <EmbedSocialWidget v-bind:ref-id="'34f1d98936ea773c28948cca33461d7e0f2e870c'"/>
   </div>
+  <div class="primary_container shadowed" align="center">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2031.342461414674!2d24.6662913162262!3d59.39399871287759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469295076c02fda1%3A0xb2cefecd8e5de659!2sUniversity%20Robotics%20Club%20NGO!5e0!3m2!1sen!2see!4v1669974480514!5m2!1sen!2see" width="1000" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
 </template>
 <script>
 import EmbedSocialWidget from '@/components/EmbedSocialWidget.vue';
