4b4afb270b0c0781f3d80719730d94ae95f36c0c
alvaht <alvaht@ttu.ee>
2022-12-06 19:03:56 +0200

    Naming difference

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <template>
   <main>
     <div class="primary-container shadowed">
+      <h1>
+        Tere!
+      </h1>
       <h3>
-        Tegemist on arenduses oleva Robotiklubi kodulehega, stabiilsuse tagamiseks külasta kas
-        <a href="https://www.robotiklubi.ee" target="_blank" rel="noopener">Praegust Kodulehte</a> või
-        <a href="http://wiki.robotiklubi.ee" target="_blank" rel="noopener">Wikit</a>.
+        Oled jõudnud TTÜ Robotiklubi kodulehele. Käesolev koduleht on hetkel veel arendusfaasis,
+        kuid meie tegevust saad jälgida meie
+        <a href="https://www.facebook.com/Robotiklubi/" target="_blank">Facebooki lehel</a> ja
+        <a href="https://www.instagram.com/robotiklubi/" target="_blank">Instagramis</a>. Lisaks saad külastada ka
+        <a href="https://www.robotiklubi.ee" target="_blank" rel="noopener">praegust kodulehte</a> või
+        <a href="http://wiki.robotiklubi.ee" target="_blank" rel="noopener">wikit</a>.
       </h3>
     </div>
   </main>
